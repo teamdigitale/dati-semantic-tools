d02c88b8b70218e6d5c2d46b3e333b0a7964a0d2
Roberto Polli <robipolli@gmail.com>
2021-10-18 17:42:15 +0200

    test oas3

--- tests/test_oas.py
+++ tests/test_oas.py
@@ -1,11 +1,15 @@
 from pathlib import Path
 
+import pytest
 from openapi_resolver.__main__ import main
 
+BASEPATH = Path(__file__).parent / "data"
 
-def test_bundle_oas():
-    dst = Path("../openapi/openapi-bundled.yaml")
+
+@pytest.mark.parametrize("oas_yaml", BASEPATH.glob("*.oas3.yaml"))
+def test_bundle_oas(oas_yaml):
+    dst = oas_yaml.with_suffix(".out.yaml")
     if dst.is_file():
         dst.unlink()
-    main("openapi/openapi.yaml", dst.absolute())
+    main(oas_yaml, dst.absolute())
     assert dst.is_file()
