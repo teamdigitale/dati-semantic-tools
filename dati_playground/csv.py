07feda4bf8488504360bd82fab7fb5f84fad7232
Roberto Polli <robipolli@gmail.com>
2022-07-20 00:29:14 +0200

    fix docker-compose-test.yaml build

--- dati_playground/csv.py
+++ dati_playground/csv.py
@@ -5,8 +5,6 @@
 import json
 import logging
 import re
-from pathlib import Path
-
 
 log = logging.getLogger(__name__)
 
@@ -24,24 +22,20 @@ def is_csv(fpath):
     report = validate_resource(fpath)
     current_errors = {}
     if not report.valid:
-        current_errors = report.flatten(
-            ["rowPosition", "fieldPosition", "code"]
-        )
+        current_errors = report.flatten(["rowPosition", "fieldPosition", "code"])
         log.error(f"Invalid file: {fpath}.")
         log.debug(json.dumps(current_errors, indent=2))
         errors.append({fpath.as_posix(): current_errors})
         raise ValueError(errors)
 
     for field_name in [
-        field.name
-        for tasks in report.tasks
-        for field in tasks.resource.schema.fields
+        field.name for tasks in report.tasks for field in tasks.resource.schema.fields
     ]:
         if not RE_FIELD.match(str(field_name)):
             log.error(f"Invalid field name: {field_name} in {fpath.name}")
             current_errors = {field_name: f"Invalid field name: {field_name}"}
 
-    if current_errors:        
+    if current_errors:
         errors.append({fpath.as_posix(): current_errors})
         raise ValueError(errors)
 
